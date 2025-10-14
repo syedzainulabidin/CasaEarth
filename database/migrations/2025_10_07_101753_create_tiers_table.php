94cf83b94386bca2ae9397ad9e04aa31f354abc4
Zain <syedzainulabidinali@gmail.com>
2025-10-14 17:50:46 +0500

    Tier Price Format

--- database/migrations/2025_10_07_101753_create_tiers_table.php
+++ database/migrations/2025_10_07_101753_create_tiers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tiers', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->integer('price');
+            $table->float('price');
             $table->json('includes');
             $table->timestamps();
         });
